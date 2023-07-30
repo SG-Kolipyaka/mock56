92fe05912aa94a2dc8d95ebad88e8309a2fcb621
sumit <sumitkolipyaka99@gmail.com>
2023-07-30 15:03:12 +0530

    Done with back

--- backend/Middlewares/upload.js
+++ backend/Middlewares/upload.js
@@ -3,9 +3,10 @@ const multer=require("multer")
 
 
 const storage=multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,"uploads/")
-    },
+  destination: function (req, file, cb) {
+    const uploadPath = path.join(__dirname, "uploads");
+    cb(null, uploadPath);
+  },
     filename:function(req,file,cb){
         let ext=path.extname(file.originalname)
         cb(null,Date.now()+ext)
